19cf7b7c110b3b43856bc4060033a278f1e7f796
Jackli77 <56607244+Jackli77@users.noreply.github.com>
2021-01-25 14:28:03 +0100

    Update scout.py

--- scout.py
+++ scout.py
@@ -1,15 +1,16 @@
 import time
-
+import random.randrange
 import pyautogui
 
 maps = {
-    "geant": [681, 763, 792, 877],
-    "mine": [613, 843, 528, 739],
-    "mine2": [1117, 758, 1240, 667],
-    "morgana": [713, 525, 839, 638],
-    "morgana2": [1026, 661, 1172, 552], #si 3ieme piege 1120 , 607
-    "crypte": [731, 730, 825, 827],
-    "crypte2": [1130, 735, 1202, 812],
+    "geantSW": [681, 763, 792, 877],
+    "geantSE": [1117, 758, 1240, 667],
+    "mineSW": [613, 843, 528, 739],
+    "mineSE": [1117, 758, 1240, 667],
+    "morganaSW": [713, 525, 839, 638],
+    "morganaSE": [1026, 661, 1172, 552], #si 3ieme piege 1120 , 607
+    "crypteSW": [731, 730, 825, 827],
+    "crypteSE": [1130, 735, 1202, 812],
 }
 
 q = True
@@ -22,11 +23,11 @@ if inf in maps.keys():
     print("Bonne map!")
     try:
         while q:
-            pyautogui.moveTo(x=maps[inf][0], y=maps[inf][1], duration=0.5)
+            pyautogui.moveTo(x=maps[inf][0]+randrange(-10,11), y=maps[inf][1]+randrange(-10,11), duration=0.5)
             time.sleep(0.1)
             pyautogui.press("z")
             time.sleep(4.2)
-            pyautogui.moveTo(x=maps[inf][2], y=maps[inf][3], duration=0.5)
+            pyautogui.moveTo(x=maps[inf][2]+randrange(-10,11), y=maps[inf][3]+randrange(-10,11), duration=0.5)
             time.sleep(0.1)
             pyautogui.press("z")
             time.sleep(4.2)
